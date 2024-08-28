e56c5d194838f4247ab393e024fa85b314b2e5e3
MichalWlk <michal.walek@o2.pl>
2024-08-28 23:44:17 +0200

    updated gitignore

---
+++ rtl/sseg_Ctrl.sv
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Michal Walek
+// 
+// Create Date: 08/02/2024 10:03:50 PM          
+// Design Name: 
+// Module Name: sseg_Ctrl
+// Project Name: 
+// Target Devices: Basys3
+// Tool Versions: 
+// Description: Structural module for 7-segment display
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module JSTK2_SPI_interface(
+    input clk,
+    input rst,
+    input x_val,
+    output logic [3:0] sseg_an,
+    output logic [6:0] sseg_char
+    );
+    
+   
+
+endmodule
+
+//
+//                 __
+//                / _)
+//       _.----._/ /
+//      /         /
+//   __/ (  | (  |
+//  /__.-'|_|--|_|
+//
